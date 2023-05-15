221ed103d0510e6ea6bc1974258b82d85f55057a
Kobe <68458245+sjh50200@users.noreply.github.com>
2023-05-15 22:48:36 +0900

    Time: 131 ms (24.75%), Space: 44.3 MB (86.90%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.ts
@@ -0,0 +1,11 @@
+function twoSum(nums: number[], target: number): number[] {
+    const res: number[] = [];
+    
+    for (let i = 0; i < nums.length - 1; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if(nums[i] + nums[j] === target) res.push(i, j);
+        }
+    }
+             
+    return res;
+};
