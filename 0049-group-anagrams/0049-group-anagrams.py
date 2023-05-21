0c25ddec4ae531ae473467f8ff918c309b92eaf9
VaporSiriz <61943747+VaporSiriz@users.noreply.github.com>
2023-05-21 16:10:15 +0900

    Time: 112 ms (53.99%), Space: 19.9 MB (43.44%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -2,10 +2,7 @@ class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         groups = {}
         for string in strs:
-            anagram = {}
-            for s in string:
-                anagram[s] = 1 if s not in anagram else anagram[s]+1
-            key = "".join([f"{k}{anagram[k]}" for k in sorted(anagram.keys())])
+            key = "".join(sorted(string))
             if key not in groups:
                 groups[key] = [string]
             else:
