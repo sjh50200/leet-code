3b63ba8fe914cfa460ece3317afb0a486a3d929c
Kobe <68458245+sjh50200@users.noreply.github.com>
2023-06-08 22:27:46 +0900

    Time: 128 ms (67.44%), Space: 51.7 MB (94.19%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.ts
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.ts
@@ -5,23 +5,23 @@ function longestConsecutive(nums: number[]): number {
     let count = 1;
     const sequenceCountArray = [];
     const sortedNums = nums.sort((next, prev) => next - prev);
-    console.log(sortedNums);
     
     sortedNums.forEach((value, index) => {
         if (index !== 0) {
             // 전 인덱스보다 1 높으면 count 증가
             if (sortedNums[index - 1] + 1 === value) {
                 count++;
-            } else if (sortedNums[index - 1] === value) {
             } else {
-                // 아니면 push하고 count 초기화
-                sequenceCountArray.push(count);
-                count = 1;
+                // value와 전 인덱스와 같으면 건너뜀
+                if (sortedNums[index - 1] !== value) {
+                    // 아니면 push하고 count 초기화
+                    sequenceCountArray.push(count);
+                    count = 1;
+                }
             }
         }
         if (index === sortedNums.length - 1) sequenceCountArray.push(count);
     });
-    console.log(sequenceCountArray)
     
     const resultArray = sequenceCountArray.sort((next, prev) => next - prev);
     const result = resultArray.pop();
